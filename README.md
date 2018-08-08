bd2a46cfff0d69c254fdd3c37f4a06e6948f1dfc
crazyczy <crazyczy@live.cn>
2018-08-08 13:56:07 +0800

    1.0 done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # musicbot
 Play music from Slack: Raspberry Pi + Netease Cloud Music
+
+Welcome!
+You can use the following command to operate the musicbot:
+
+1. play {key} # search song by keyword and play it
+2. play id {id} # id is 108297 in `https://music.163.com/#/song?id=108297`
+3. replay # continue play song after pause it
+4. pause
+5. stop
+6. help
+
+## TODO
+
+1. song list
--- functions.py
+++ functions.py
@@ -2,6 +2,11 @@
 import requests
 import json
 
+from player import Player
+
+
+player = Player()
+
 
 def get_real_url_from_id(id):
     '''
@@ -19,6 +24,22 @@ def get_real_url_from_id(id):
     return r_dict['data'][0]['url']
 
 
+def get_detail_from_id(id):
+    '''
+        Get song name and artist
+    '''
+
+    url = f'https://api.imjad.cn/cloudmusic/?type=detail&id={id}'
+    r = requests.get(url)
+    r_dict = json.loads(r.text)
+    if not r_dict['songs']:
+        print(f'Incorrect song id: {id}')
+        return None, None
+    name, artist = r_dict['songs'][0]['name'], r_dict['songs'][0]['ar'][0]['name']
+
+    return name, artist
+
+
 def search_songs(key, limit=5, page=1):
     '''
         Search songs by keyword
@@ -32,3 +53,50 @@ def search_songs(key, limit=5, page=1):
             'id': item['id'],
             'artist': item['artists'][0]['name']
         } for item in r_dict['songList']]
+
+
+def play_by_id(id):
+    url = get_real_url_from_id(id)
+    if not url:
+        return 'Incorrect id or song has been removed'
+
+    name, artist = get_detail_from_id(id)
+    player.set_player(url)
+    player.play()
+    return f':musical_note: {name}-{artist}'
+
+def play_by_key(key):
+    song_list = search_songs(key)
+    for song in song_list:
+        url = get_real_url_from_id(song['id'])
+        if not url:
+            continue
+        return play_by_id(song['id'])
+    return 'Not found'
+
+def replay():
+    player.play()
+    return 'Music is play now'
+
+
+def pause():
+    player.pause()
+    return 'Music is pause now'
+
+
+def stop():
+    player.stop()
+    return 'Music is stop now'
+
+def help():
+    help_text = '''
+Welcome!
+You can use the following command to operate the musicbot:
+1. play {key} # search song by keyword and play it
+2. play id {id} # id is 108297 in `https://music.163.com/#/song?id=108297`
+3. replay # continue play song after pause it
+4. pause
+5. stop
+6. help
+'''
+    return help_text
